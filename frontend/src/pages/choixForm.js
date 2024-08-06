85ba27393373d5e683434cf10e03d1864982cde0
Dupouy Lucie <luciedupouy@ensc.fr>
2024-08-06 18:32:10 -0400

    essai1000

--- frontend/src/pages/choixForm.js
+++ frontend/src/pages/choixForm.js
@@ -40,7 +40,7 @@ const FormSelection = ({ userId, resetUserId }) => {
 
   const saveAndContinueLater = async () => {
     try {
-      const validFieldsResponse = await axios.get(' https://backend-flax-theta.vercel.app/get_valid_fields');
+      const validFieldsResponse = await axios.get(' https://backend-pntjk8nqq-luciedupouys-projects.vercel.app//get_valid_fields');
       const validFields = validFieldsResponse.data.map(field => field.original_field_name);
 
       const validAnswers = Object.entries(answers).reduce((acc, [key, value]) => {
@@ -52,7 +52,7 @@ const FormSelection = ({ userId, resetUserId }) => {
 
       console.log("Données envoyées :", { id: userId, ...validAnswers });
 
-      const response = await axios.post(' https://backend-flax-theta.vercel.app/update', {
+      const response = await axios.post(' https://backend-pntjk8nqq-luciedupouys-projects.vercel.app//update', {
         id: userId,
         ...validAnswers
       });
@@ -74,7 +74,7 @@ const FormSelection = ({ userId, resetUserId }) => {
 
   const finishForm = async () => {
     try {
-      const validFieldsResponse = await axios.get(' https://backend-flax-theta.vercel.app/get_valid_fields');
+      const validFieldsResponse = await axios.get(' https://backend-pntjk8nqq-luciedupouys-projects.vercel.app//get_valid_fields');
       const validFields = validFieldsResponse.data.map(field => field.original_field_name);
 
       const validAnswers = Object.entries(answers).reduce((acc, [key, value]) => {
@@ -86,7 +86,7 @@ const FormSelection = ({ userId, resetUserId }) => {
 
       console.log("Données envoyées :", { id: userId, ...validAnswers });
 
-      const response = await axios.post(' https://backend-flax-theta.vercel.app/update', {
+      const response = await axios.post(' https://backend-pntjk8nqq-luciedupouys-projects.vercel.app//update', {
         id: userId,
         ...validAnswers
       });
